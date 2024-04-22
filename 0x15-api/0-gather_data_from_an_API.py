841fa577d843365455dd1d49054afccaa66a74f8
Eltrone <amineelorche@gmail.com>
2024-04-22 15:23:28 -0700

    file added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,33 @@
 #!/usr/bin/python3
-""" Script using a REST API, for given employee ID """
+""" Script to fetch tasks of all employees and export them to a JSON file """
 
+import json
 import requests
-import sys
+
+def fetch_all_data():
+    """ Fetches data of all users and their tasks using the REST API """
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users").json()
+    todos = requests.get(base_url + "todos").json()
+
+    user_dict = {user['id']: user['username'] for user in users}
+    tasks_dict = {user_id: [] for user_id in user_dict.keys()}
+
+    for todo in todos:
+        task_info = {
+            "username": user_dict[todo["userId"]],
+            "task": todo["title"],
+            "completed": todo["completed"]
+        }
+        tasks_dict[todo["userId"]].append(task_info)
+
+    return tasks_dict
+
+def export_all_to_json():
+    """ Exports all tasks of all employees to a JSON file """
+    tasks_dict = fetch_all_data()
+    with open("todo_all_employees.json", 'w') as jsonfile:
+        json.dump(tasks_dict, jsonfile)
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_data = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
-    todo = requests.get(
-        base_url + "todos",
-        params={"userId": sys.argv[1]}
-    ).json()
-
-    comp = [t.get("title") for t in todo if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(comp), len(todo)))
-    [print("\t {}".format(c)) for c in comp]
+    export_all_to_json()
