c608f2ee8b18a08553d6ff949d0891cb53eb666a
Eltrone <amineelorche@gmail.com>
2024-04-22 06:20:51 -0700

    file added

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,18 +6,18 @@ import sys
 
 if __name__ == "__main__":
     # Base URL for the API
-    baseurl = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/"
 
     # Extract user ID from command line argument
     user_id = sys.argv[1]
 
     # Retrieve user data
-    user_response = requests.get(baseeurl + "users/{}".format(user_id))
+    user_response = requests.get(url + "users/{}".format(user_id))
     user_data = user_response.json()
     username = user_data.get("username")
 
     # Retrieve todo data for the user
-    todo_response = requests.get(baseurl + "todos", params={"userId": user_id})
+    todo_response = requests.get(url + "todos", params={"userId": user_id})
     todo_data = todo_response.json()
 
     # Write data to CSV file
