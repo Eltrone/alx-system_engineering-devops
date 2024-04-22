445216fb44225cf027b121704c9222761a8da1ec
Eltrone <amineelorche@gmail.com>
2024-04-22 05:59:33 -0700

    file added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,9 +7,9 @@ import sys
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
     user_data = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
-    todo_data = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
+    todo = requests.get(base_url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed_tasks = [task.get("title") for task in todo_data if task.get("completed")]
+    comp = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(completed_tasks), len(todo_data)))
-    [print("\t{}".format(task)) for task in completed_tasks]
+        user_data.get("name"), len(comp), len(todo)))
+    [print("\t {}".format(c)) for c in comp]
