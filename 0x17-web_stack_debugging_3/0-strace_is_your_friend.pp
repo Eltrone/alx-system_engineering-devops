994e923a3344f7b10436bf26384b12081e462fe6
Eltrone <amineelorche@gmail.com>
2024-05-12 11:33:36 +0000

    file added

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,6 @@
 # Fix Apache error 500 for WordPress on LAMP
 
-exec { 'replace_php_string':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  unless  => 'grep -q "php" /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin'],
+exec {'replace_php_string':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
-
